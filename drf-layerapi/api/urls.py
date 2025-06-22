59d0860d19154e7ce9ce2bc33f6e75b387d3ab62
nOester <nils.oesterling@swisstopo.ch>
2025-06-22 10:14:43 +0200

    cleanup

--- drf-layerapi/api/urls.py
+++ drf-layerapi/api/urls.py
@@ -1,18 +1,7 @@
 from django.urls import path
-from .views import LayerUploadView, HealthCheck #health_check
+from .views import LayerUploadView, HealthCheck
 
 urlpatterns = [
     path('upload/', LayerUploadView.as_view(), name='layer-upload'),
-    # path('health/', health_check, name='health-check'),  # dein bestehender GET-Endpunkt
     path('health/', HealthCheck.as_view(), name='health-check'),
 ]
-
-
-""" from django.urls import path
-from .views import health_check, upload_layer_file
-
-urlpatterns = [
-    path('health/', health_check, name='health_check'),
-    path('upload/', upload_layer_file, name='upload_layer_file'),
-]
- """
